670c301d881dae823da62a2dd010270f0330b9ee
m.tejero@e-coordina.com <m.tejero@e-coordina.com>
2020-10-28 14:33:23 +0100

    Test changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>Naice</code>.
+           <code>Nice</code>.
         </p>
         <a
           className="App-link"
